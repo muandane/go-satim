44922fdb6a2ead50f268d0304802f40cfbb181f0
Zine <zine@omnivya.fr>
2026-08-30 15:32:58 +0200

    chore: remove legacy PHP files and update .gitignore for Go

--- src/Exception/SatimInvalidArgumentException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PiteurStudio\Exception;
-
-use InvalidArgumentException;
-
-class SatimInvalidArgumentException extends InvalidArgumentException {}
--- src/Exception/SatimInvalidDataException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PiteurStudio\Exception;
-
-use Exception;
-
-class SatimInvalidDataException extends Exception {}
